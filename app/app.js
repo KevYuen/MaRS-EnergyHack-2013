566e096f01685a14df83a167944320fbeed49ba3
Kevin <kevin@Kevins-MacBook-Air.local>
2013-09-07 15:10:00 -0400

    added template layout

--- app/app.js
+++ app/app.js
@@ -1,11 +1,21 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    path = require('path');
 
-app.get('/app/', function(req, res) {
-    res.json({
-        hello: 'world'
-    });
+app.configure(function(){
+ 	app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+ 	  app.use(express.bodyParser());
+  	app.use(express.methodOverride());
+  	app.use(app.router);
+  	app.use(express.static(path.join(__dirname, 'public')));
+  	
 });
+
+var routes = require('./controllers/routes.js');
+
+//routes
+app.get('/app/', routes.index);
 
 app.listen(3000);
 console.log('Listening on port 3000');
