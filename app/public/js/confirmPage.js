6807ca928bdc97ed038e997b893c18e73810f828
Kevin <kevin@Kevins-MacBook-Air.local>
2013-09-08 09:26:46 -0400

    removed extra bracket

--- app/public/js/confirmPage.js
+++ app/public/js/confirmPage.js
@@ -10,7 +10,6 @@ function confirm(){
 		if (data.status == 200){
 			$.post('http://ec2-184-73-71-236.compute-1.amazonaws.com/analytics', {id: data.id}).done(function(){
 					window.close();
-				}
 			});
 		}else{
 			alert("An error has occured, please try again!");
