af343f98e81f55557a9b9f11813487a30c31b5a2
Kevin <kevin@Kevins-MacBook-Air.local>
2013-09-07 21:05:09 -0400

    changed to use server url

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,8 +1,8 @@
 function loginPopup(){
 	var phoneNumber = $('#phone-number').val(),
 		client_id = "id-e0025d35-eb9a-474d-a91b-5eb10a0fbca7",
-		//return_url = "http://ec2-184-73-71-236.compute-1.amazonaws.com/callback?phoneNumber=";
-		return_url = "http://localhost:3000/callback?phoneNumber=";
+		return_url = "http://ec2-184-73-71-236.compute-1.amazonaws.com/callback?phoneNumber=";
+		//return_url = "http://localhost:3000/callback?phoneNumber=";
 
 	phoneNumber = replaceAll("-", "", phoneNumber);
 	if (phoneNumber.length != 10 && !(/^[0-9]+$/.test(phoneNumber))){
